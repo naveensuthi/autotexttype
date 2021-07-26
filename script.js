943c352e43db069ad5e37afff0a3602eef4738d1
naveensuthi <60281493+naveensuthi@users.noreply.github.com>
2021-07-26 14:39:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const textEl = document.getElementById("text");
 const speedEl = document.getElementById("speed");
-const text = "We Love Programming";
+const text = "Suthi Naveen Kumar";
 
 let idx = 1;
 let speed = 300 / speedEl.value;
